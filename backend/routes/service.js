0f4da12da4908125124ab50537002cb0ee253098
RUX-OTP <w4025350@gmail.com>
2025-09-22 09:29:56 +0100

    Update service.js

--- backend/routes/service.js
+++ backend/routes/service.js
@@ -1,1 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const { sendOTP } = require('../controllers/serviceController');
 
+router.post('/send', sendOTP);
+
+module.exports = router;
