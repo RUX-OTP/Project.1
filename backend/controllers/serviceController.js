db4be3969aab5874b0faec8529b9978c95beab64
RUX-OTP <w4025350@gmail.com>
2025-09-22 09:37:18 +0100

    Update serviceController.js

--- backend/controllers/serviceController.js
+++ backend/controllers/serviceController.js
@@ -1,12 +1,12 @@
 const twilio = require('twilio');
-const client = twilio(process.env.AC41269f9f2ab722bb198842c3b185a66d, process.env.ba8e624cdef6c265e44fae5496abd02e);
+const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
 
 const sendOTP = async (req, res) => {
   const { to, message } = req.body;
   try {
     const response = await client.messages.create({
       body: message,
-      from: process.env.+14026215872,
+      from: process.env.TWILIO_NUMBER,
       to: to
     });
     res.json({ success: true, sid: response.sid });
