41783419a53a34592bdc95a24177f840ab477423
RUX-OTP <w4025350@gmail.com>
2025-09-22 09:19:59 +0100

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,1 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+const Navbar = () => (
+  <nav>
+    <Link to="/">Home</Link> | 
+    <Link to="/service">Service</Link> | 
+    <Link to="/login">Login</Link> | 
+    <Link to="/signup">Signup</Link> | 
+    <Link to="/admin">Admin</Link>
+  </nav>
+);
+
+export default Navbar;
