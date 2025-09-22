bd86272cae7af09c917752b9044079cb1535eddb
RUX-OTP <w4025350@gmail.com>
2025-09-22 09:18:11 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Service from './pages/Service';
+import AdminPanel from './pages/AdminPanel';
+import Navbar from './components/Navbar';
 
+const App = () => (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/service" element={<Service />} />
+      <Route path="/admin" element={<AdminPanel />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
